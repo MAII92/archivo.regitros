2162a82abc3f20a5326d978893dafae545c20732
maguilarm <maguilarm@WSAMZN-B7KI5DCU>
2023-05-15 17:38:59 -0500

    Add project files.

---
+++ archivo.regitros/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class CerosRellenar
+{
+    public static string Left(long number, int rellenar)
+    {
+        string cadena = number.ToString().PadLeft(rellenar, '0');
+        return cadena;
+    }
+    public static string Right(long number, int rellenar)
+    {
+        string cadena = number.ToString().PadRight(rellenar, '0');
+        return cadena;
+    }
+}
+}
---
+++ archivo.regitros/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARCHIVO
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingresa un registro:");
+            String NumRegistros = Console.ReadLine();
+            if (String.IsNullOrEmpty(NumRegistros))
+            {
+                Console.WriteLine("No se ingresó ningún registro.");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Ingresa registro de control :");
+                String filename = Console.ReadLine();
+                if (String.IsNullOrEmpty(filename))
+                {
+                    Console.WriteLine("No se ingresó el registro de control.");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Int32 repeticiones = Int32.Parse(NumRegistros);
+                    String Tregistro, Nidentificacion, Cidentidad, fecha, hora, reservado, septimo, octavo, noveno, decimo, once, linea = "";
+                    for (Int32 i = 1; i <= repeticiones; i++)
+                    {
+                     Tregistro Left(2, 0,1);
+                     Nidentificacion Left(RandomNumber.Generar(), 16);
+                     Cidentidad = "3";
+                     fecha = FechaRandom.Fecha();
+                     reservado = CerosRellenar.Left(RandomNumber.Generar(), 170);
+                     Console.WriteLine(date1.ToString("hh:mm:ss.F", ci));
+
+                    linea += Tregistro + Nidentificacion + Cidentidad + fecha + hora + reservado +;
+                    linea += "\n";
+                    try
+                    {
+                        using (StreamWriter writer = new StreamWriter(@"D:\Users\maguilarm\source\repos\archivo\archivo\" + filename + ".txt"))
+                        {
+                            writer.Write(linea);
+
+                        }
+
+
+
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
+}
